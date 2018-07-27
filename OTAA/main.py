8acc2d06981133d334fc237c3b6ea4079f1b132c
Meikaefer <berger-meike@web.de>
2018-07-27 15:19:59 +0200

    OTAA Fehlerbehebung
    Fehler wurde behoben. OTTA funktioniert.

--- OTAA/main.py
+++ OTAA/main.py
@@ -1,4 +1,3 @@
-from machine import RTC
 from network import LoRa
 import socket
 import time
@@ -8,24 +7,22 @@ import ubinascii
 # Please pick the region that matches where you are using the device:
 # Asia = LoRa.AS923
 # Australia = LoRa.AU915
- Europe = LoRa.EU868
+# Europe = LoRa.EU868
 # United States = LoRa.US915
 lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
 
 # create an OTAA authentication parameters
 app_eui = ubinascii.unhexlify('70B3D57ED0010C40')
 dev_eui = ubinascii.unhexlify('0075B931167C88BA')
-app_key = ubinascii.unhexlify('9CC0DBA4C42C64215FA9CE0C0E2D5F83')
-
-rtc = RTC(id=0)
+app_key = ubinascii.unhexlify('6057C8254E92296140F68E8B942AB6C3')
 
 # join a network using OTAA (Over the Air Activation)
 lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
 
 # wait until the module has joined the network
 while not lora.has_joined():
-#    print("not yet joined")
     time.sleep(2.5)
+#    print('Not yet joined...')
 
 # create a LoRa socket
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
@@ -38,9 +35,7 @@ s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 s.setblocking(True)
 
 # send some data
-s.send(bytes([0x01, 0x02, 0x04]))
-
-#s.send(rtc.now())
+s.send(bytes([0x01, 0x02, 0x03]))
 
 # make the socket non-blocking
 # (because if there's no data received it will block forever...)
