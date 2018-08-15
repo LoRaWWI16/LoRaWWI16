860e17fc0f33f8fd3adbe5c0b894597e6bd1e6fd
Alexej <alexej@mendler.de>
2018-08-15 20:55:38 +0200

    Wlan-RTC
    code for WLAN with RTC

---
+++ Wlan-RTC/boot.py
@@ -0,0 +1,31 @@
+import machine
+import pycom
+#import persistence
+from network import WLAN
+from machine import WDT
+#from helpers import *
+
+ #bluetooth = Bluetooth()
+ #bluetooth.deinit()
+ # wlan = WLAN(mode=WLAN.STA)
+ # wlan.deinit()
+
+########################################
+## Handle wake by button to enter debug mode
+########################################
+
+# if machine.wake_reason()[0] is machine.PIN_WAKE:
+#     print('PIN WAKE')
+#     connect_to_WLAN()
+# else:
+#     watchdog_timer = None
+#
+#     if machine.reset_cause() not in [machine.DEEPSLEEP_RESET, machine.SOFT_RESET, machine.WDT_RESET]:
+#         print('hard reset, erasing NVS')
+#         pycom.nvs_erase_all()
+#         persistence.cleanup()
+#     else:
+#         print('soft/deepsleep reset, enabling watchdog timer')
+#         watchdog_timer = WDT(timeout=30000)
+#
+#     machine.main('main.py')
